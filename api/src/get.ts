b2ecfe7bd0af79c35ff5a31361e0007bf2054a9d
tyler <tyler@cruftbusters.com>
2022-08-29 16:02:39 -0700

    get.ts pumps all jobs to api

--- api/src/get.ts
+++ api/src/get.ts
@@ -1,18 +1,45 @@
 import axios from 'axios'
 
+const target = process.env.TARGET || 'http://localhost:8080'
+
 async function main() {
-  const response = await get()
-  const payload = extractPayload(response.data)
-  process.stdout.write(JSON.stringify(payload))
+  let offset = 0
+  let count
+  do {
+    console.error(`getting results at offset ${offset}`)
+    const response = await retry(() => get(offset))
+    const data = extractPayload(response.data)
+    await axios.post(target, data, {
+      headers: { 'Content-Type': 'application/json' },
+    })
+
+    count = JSON.parse(data).eagerLoadRefineSearch.data.jobs.length
+    offset += count
+  } while (count > 0)
 }
 
-function get() {
+function get(offset: number) {
   return axios.get('https://careers.microsoft.com/us/en/search-results', {
     params: new URLSearchParams({
-      from: '20',
+      from: offset.toString(),
       s: '1',
     }),
   })
+}
+
+async function retry<T>(fn: () => Promise<T>) {
+  for (let i = 0; ; i++) {
+    try {
+      return await fn()
+    } catch (e) {
+      if (i < 8) {
+        const wait = 250 * Math.pow(2, i)
+        console.error(`retrying in ${wait}ms`)
+        await new Promise((resolve) => setTimeout(resolve, wait))
+        continue
+      } else throw e
+    }
+  }
 }
 
 function extractPayload(data: string) {
@@ -24,7 +51,7 @@ function extractPayload(data: string) {
   if (!line) throw Error('Could not find payload start marker')
   line = line.split(startMarker)[1]
   line = line.split(endMarker)[0]
-  return JSON.parse(line)
+  return line
 }
 
 main()
