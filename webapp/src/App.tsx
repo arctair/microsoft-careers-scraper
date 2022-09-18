3ff47154a3792a1fa3b3dcbcceee5c466b59ec6d
tyler <tyler@cruftbusters.com>
2022-09-17 18:52:13 -0700

    webapp - add verbose field and ShowHide component

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { context, Provider } from './StateV2'
 
 export default function App() {
@@ -42,8 +42,62 @@ function Job({ jobId }: JobProps) {
   return (
     <>
       <h3>{latest.title}</h3>
-      {latest.descriptionTeaser}
+      <h4>{latest.location}</h4>
+      <ShowHide
+        content={latest.descriptionTeaser}
+        display="block"
+        label="teaser"
+      />
+      <ShowHide content={latest.jobSummary} label="job summary" />
+      <ShowHide content={latest.description} label="description" />
+      <ShowHide
+        content={latest.jobResponsibilities}
+        label="job responsibilities"
+      />
+      <ShowHide
+        content={latest.jobQualifications}
+        label="job qualifications"
+      />
       <span style={{ display: 'none' }}>{JSON.stringify(latest)}</span>
+    </>
+  )
+}
+
+type ShowHideProps = {
+  content: string
+  display?: 'none' | 'block'
+  label: string
+}
+function ShowHide({
+  content,
+  display: defaultDisplay = 'none',
+  label,
+}: ShowHideProps) {
+  const [display, setDisplay] = useState(defaultDisplay)
+  return (
+    <>
+      <button
+        onClick={() =>
+          setDisplay((display) => (display === 'none' ? 'block' : 'none'))
+        }
+        style={{
+          color: display === 'none' ? 'gray' : 'black',
+          border: 'none',
+          marginRight: '0.5rem',
+        }}
+      >
+        {label}
+      </button>
+      <div
+        style={{
+          display,
+          borderTop: '1px solid gray',
+          borderBottom: '1px solid gray',
+          margin: '0.5rem -0.5rem',
+          padding: '0.5rem',
+        }}
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
     </>
   )
 }
